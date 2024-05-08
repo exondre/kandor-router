4d42ad732fcfa25452637a59bbcdd6bc766bb3e8
Edixon Gutierrez Marmol <dev@eagmarmol.cl>
2024-05-08 19:20:58 -0400

    👔 adds redirect cases for social media

--- src/app/redirect/redirect.component.ts
+++ src/app/redirect/redirect.component.ts
@@ -20,6 +20,15 @@ export class RedirectComponent implements OnInit {
           case 'ONBOARDING_INFO':
             window.location.href = 'https://drive.google.com/file/d/15iB3SdawaGQ8XvVTi8UgEmkOkzPqw7sb/view';
             break;
+          case 'IG':
+            window.location.href = 'https://www.instagram.com/kandorfit';
+            break;
+          case 'WA':
+            window.location.href = 'https://api.whatsapp.com/send?phone=56968369872&text=%C2%A1Hola!%20Quiero%20informaci%C3%B3n%20acerca%20de%20c%C3%B3mo%20ser%20parte%20de%C2%A0Kandor%C2%A0%F0%9F%92%AA%F0%9F%8F%8B';
+            break;
+          case 'TK':
+            window.location.href = 'https://www.tiktok.com/@kandorfitness';
+            break;
           // Agregar más casos según sea necesario
           case 'HIDDEN_HOME':
             this.router.navigate(['hidden_home']);
